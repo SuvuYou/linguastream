23c9baa8ecb305ba1faf0a1f7cbcd5d544cf329c
Nazar Syvuliak <Nazarsuvyliak1@gmail.com>
2026-04-07 23:44:06 +0200

    fetch unregistered content from jellyfin

--- components/features/LibraryGrid.tsx
+++ components/features/LibraryGrid.tsx
@@ -1,21 +1,18 @@
 "use client";
 
 import Image from "next/image";
-import type { MediaContent } from "@prisma/client";
-import type { JellyfinItem } from "@/lib/jellyfin.client";
+import type { MergedContentItem } from "@/types";
 import Link from "next/link";
 import AddToLibraryModal from "./AddToLibraryModal";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function LibraryGrid({
-  libraryItems,
+  mergedContentItem,
   isAdmin,
-  publicMediaContentMap,
 }: {
-  libraryItems: (JellyfinItem & { thumbnailUrl: string })[];
+  mergedContentItem: MergedContentItem[];
   isAdmin?: boolean;
-  publicMediaContentMap: Map<string, MediaContent>;
 }) {
   const router = useRouter();
   const [modal, setModal] = useState<{ id: string; title: string } | null>(
@@ -25,19 +22,31 @@ export default function LibraryGrid({
   return (
     <>
       <div className="grid grid-cols-3 border-l border-t border-primary-border">
-        {libraryItems.map((item) => {
-          const publicMediaContent = publicMediaContentMap.get(item.Id);
+        {mergedContentItem.map((item) => {
+          if (!item.jellyfinItem) {
+            return (
+              <div
+                key={item.id}
+                className="relative border-r border-b border-primary-border p-4 hover:bg-background-hover transition-colors group"
+              >
+                <div className="aspect-video bg-background mb-3 flex items-center justify-center text-secondary-text">
+                  No longer available
+                </div>
+                <div className="text-sm font-medium truncate">{item.title}</div>
+              </div>
+            );
+          }
 
           return (
             <div
-              key={item.Id}
+              key={item.jellyfinItem.Id}
               className="relative border-r border-b border-primary-border p-4 hover:bg-background-hover transition-colors group"
             >
-              <Link href={`/watch/${item.Id}`}>
+              <Link href={`/watch/${item.jellyfinItem.Id}`}>
                 <div className="aspect-video bg-background mb-3 overflow-hidden">
                   <Image
                     src={item.thumbnailUrl}
-                    alt={item.Name}
+                    alt={item.jellyfinItem.Name}
                     className="w-full h-full object-cover opacity-80 group-hover:opacity-100 transition-opacity"
                     width={1}
                     height={1}
@@ -45,14 +54,21 @@ export default function LibraryGrid({
                     unoptimized // TODO: Remove when we have a proper jellyfin proxy
                   />
                 </div>
-                <div className="text-sm font-medium truncate">{item.Name}</div>
+                <div className="text-sm font-medium truncate">
+                  {item.jellyfinItem.Name}
+                </div>
                 <div className="text-xs text-secondary-text mt-1">
-                  {item.Type}
+                  {item.jellyfinItem.Type}
                 </div>
               </Link>
-              {isAdmin && !publicMediaContent && (
+              {isAdmin && (
                 <button
-                  onClick={() => setModal({ id: item.Id, title: item.Name })}
+                  onClick={() =>
+                    setModal({
+                      id: item.jellyfinItem!.Id,
+                      title: item.jellyfinItem!.Name,
+                    })
+                  }
                   className="absolute top-2 right-2 text-xs px-2 py-1 border border-primary-border text-secondary-text hover:text-primary-text  bg-background opacity-0 group-hover:opacity-100 transition-all"
                 >
                   + Add
@@ -61,7 +77,7 @@ export default function LibraryGrid({
             </div>
           );
         })}
-        {libraryItems.length === 0 && (
+        {mergedContentItem.length === 0 && (
           <div className="col-span-3 p-12 text-center text-secondary-text text-sm">
             No items found in your Jellyfin library.
           </div>
