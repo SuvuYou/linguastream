ccbcdda312d6a3c7d81d6486e6407149247f2e36
Nazarko <user@Users-MacBook-Air.local>
2026-04-09 23:45:14 +0200

    create rount for creating and deleting user

---
+++ app/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const session = req.cookies.get("session")?.value;
+  const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
+
+  if (!session && !isAuthPage) {
+    return NextResponse.redirect(new URL("/auth/signin", req.url));
+  }
+
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
