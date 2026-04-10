9d01c47d781bd1cff62ad11920f68c750615a0fe
Nazar Syvuliak <Nazarsuvyliak1@gmail.com>
2026-04-10 13:32:08 +0200

    use proxy instead of middlewear

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const session = req.cookies.get("session")?.value;
   const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
 
