6ab6c048bc4b7d8317de251dafdc55b9c981623c
Nazarko <user@Users-MacBook-Air.local>
2026-04-14 19:44:02 +0200

    setup tests

--- components/pages/LibraryPage.tsx
+++ components/pages/LibraryPage.tsx
@@ -10,20 +10,18 @@ import LibraryGrid from "../features/LibraryGrid";
 export default function LibraryPage() {
   const userData = useUser();
 
-  const isAdmin = userData.data?.is_admin; 
+  const isAdmin = userData.data?.is_admin;
 
   return (
     <>
-      <>
-        <div className="flex items-center h-10 border-b border-primary-border px-4 gap-4">
-          <SearchBar />
-          <LanguageFilter />
-        </div>
-        <div className="ml-auto flex items-center gap-4 px-4">
-          {isAdmin && <SyncButton />}
-          {isAdmin && <UnregisteredCheckbox />}
-        </div>
-      </>
+      <div className="flex items-center h-10 border-b border-primary-border px-4 gap-4">
+        <SearchBar />
+        <LanguageFilter />
+      </div>
+      <div className="ml-auto flex items-center gap-4 px-4">
+        {isAdmin && <SyncButton />}
+        {isAdmin && <UnregisteredCheckbox />}
+      </div>
       <LibraryGrid />
       {/* TODO: Pagination controls should go here */}
     </>
